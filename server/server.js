6bee77ee14de6d8203bac62518b6b16cb65fc54a
Tiffany Ip <tiffanyip@ucla.edu>
2016-09-17 15:35:34 -0700

    [setup] setup basic server environment (#1)

---
+++ server/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+// var router = require('./config/router.js');
+
+var app = express();
+
+app.set('PORT', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('./client'));
+app.use('/lib', express.static('./node_modules'));
+app.use('/', router);
+
+app.listen(app.get('PORT'), function() {
+	console.log('Express server listening on port ', app.get('PORT'));
+});
+
+module.exports = app;
