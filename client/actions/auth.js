a96c6e38555293dc552228145009d6e328adf18d
Calvin Lee <calsmlee@uw.edu>
2016-09-20 11:29:10 -0700

    [fix] Deleted conflict text

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -3,12 +3,6 @@ import axios from 'axios';
 export function userSignUp(info) {
   console.log('inside actions folder!!!', info);
   const request = axios.post('/auth/signup', info);
-<<<<<<< 75bd2a63791f156f4002f7274a458daa7038ccca
-  console.log('request inside auth.js', request);
-  return {
-    type: 'SIGN_UP',
-    payload: request
-=======
   return (dispatch) => {
     console.log('dispatch inside auth.js', dispatch);
     return request
@@ -16,6 +10,5 @@ export function userSignUp(info) {
         console.log('inside dispatch', response.data);
         dispatch({ type: 'SIGN_UP', payload: response.data });
       });
->>>>>>> [feature] Switched from redux promise to thunk
   };
 }
