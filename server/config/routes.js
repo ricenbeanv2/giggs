4ebc261eed4aba4d9a975d19a766280672bd9a01
Tiffany Ip <tiffanyip@ucla.edu>
2016-09-20 11:56:23 -0700

    [server-feat] complete local signup and signin with bcrypt (#16)
    * [refactor] refactor createUser function to meet with eslint
    
    * [refactor] Fixed style issues
    
    * [feature] Switched from redux promise to thunk
    
    * [feature] SignUp mostly complete
    
    * [fix] Deleted conflict text
    
    * [server-feat] hash password upon creating user
    
    * [sever-feat] user bcrypt to compare password

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,8 @@
-var router = require('express').Router();
-var applicantController = require('../applicant/applicantCtrl.js');
-var categoryController = require('../category/categoryCtrl.js');
-var jobController = require('../job/jobCtrl.js');
-var userController = require('../user/userCtrl.js');
+const router = require('express').Router();
+const applicantController = require('../applicant/applicantCtrl');
+const categoryController = require('../category/categoryCtrl');
+const jobController = require('../job/jobCtrl');
+const userController = require('../user/userCtrl');
 
 router.get('/applicant', applicantController.helloWorld);
 
--- server/user/userCtrl.js
+++ server/user/userCtrl.js
@@ -1,33 +1,42 @@
 var User = require('./userModel.js');
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
-	helloWorld: function(req, res) {
-		res.send('hello users');
-	},
-
-	createUser: function(req, res){
-		var newUser = {
+	createUser: (req, res) => {
+		const newUser = {
 			username: req.body.username,
 			password: req.body.password,
 			name: req.body.name,
 			email: req.body.email
-		}
+		};
 		//check if user exists
 		User.build(newUser).save()
-		.then(function(result){
-			return res.status(200).send(newUser);
-		}).catch(function(err){
+		.then(user => {
+			bcrypt.hash(newUser.password, 10, (err, hash) => {
+				user.update({ password: hash });
+				res.status(200).send('user added!');
+			});
+		})
+		.catch(err => {
 			throw err;
 		});
-
-	},
-	authUser: function(req,res){
-		res.status(200).send('hi');
 	},
 
-	updateUser: function(req, res){
-
+	authUser: (req, res) => {
+		const userInput = {
+			username: req.query.username,
+			password: req.query.password
+		};
+		User.findOne({ where: { username: userInput.username } })
+		.then(user => {
+			bcrypt.compare(userInput.password, user.password, (err, valid) => {
+				if (err) {
+					throw err;
+				}
+				res.status(200).send(valid);
+			});
+		});
 	}
 
 };
