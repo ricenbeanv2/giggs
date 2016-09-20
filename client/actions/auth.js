17100ca696a7f2a79620142d703fbeeb6ab54774
Calvin Lee <calsmlee@uw.edu>
2016-09-20 12:21:06 -0700

    [refactor] Deleted some console logs

--- client/actions/auth.js
+++ client/actions/auth.js
@@ -1,26 +1,20 @@
 import axios from 'axios';
 
 export function userSignUp(info) {
-  console.log('inside actions folder!!!', info);
   const request = axios.post('/auth/signup', info);
   return (dispatch) => {
-    console.log('dispatch inside auth.js', dispatch);
     return request
       .then((response) => {
-        console.log('inside dispatch', response.data);
         dispatch({ type: 'SIGN_UP', payload: response.data });
       });
   };
 }
 
 export function userSignIn(info) {
-  console.log('inside actions folder!!!', info);
   const request = axios.get('/auth/signin', { params: info });
   return (dispatch) => {
-    console.log('dispatch inside auth.js', dispatch);
     return request
       .then((response) => {
-        console.log('inside dispatch', response.data);
         dispatch({ type: 'SIGN_IN', payload: response.data });
       });
   };
