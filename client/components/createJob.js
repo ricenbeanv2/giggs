e29bccea1bfbbba27ee3dfc86beb4b3193df8ed6
Calvin Lee <calsmlee@uw.edu>
2016-09-22 16:27:41 -0700

    [feature] Refactored signUp and createJob to stateless functional components

--- client/components/createJob.js
+++ client/components/createJob.js
@@ -6,56 +6,55 @@ import SelectionComponent from './selectionComponent';
 import { sendJob } from '../actions/jobs';
 import renderField from './renderField';
 
-class CreateJobForm extends Component {
-  render() {
-    const { error, handleSubmit, submitting } = this.props;
-    const categories = [
-      { value: 'plumbing', label: 'Plumbing' },
-      { value: 'tutor', label: 'Tutor' },
-      { value: 'tech', label: 'Tech' },
-      { value: 'auto', label: 'Auto' },
-      { value: 'cook', label: 'Cook' }
-    ];
-    return (
-      <form onSubmit={handleSubmit(this.props.sendJob)}>
-        <h3>Create Job</h3>
-        <div className="form-group">
-          <label>Job Name</label>
-          <Field name="jobName" component={renderField} type="text" className="form-control" />
-        </div>
+let CreateJobForm = props => {
+  const { error, handleSubmit, submitting } = props;
+  const categories = [
+    { value: 'plumbing', label: 'Plumbing' },
+    { value: 'tutor', label: 'Tutor' },
+    { value: 'tech', label: 'Tech' },
+    { value: 'auto', label: 'Auto' },
+    { value: 'cook', label: 'Cook' }
+];
+  return (
+    <form onSubmit={handleSubmit(props.sendJob)}>
+      <h3>Create Job</h3>
+      <div className="form-group">
+        <label>Job Name</label>
+        <Field name="jobName" component={renderField} type="text" className="form-control" />
+      </div>
 
-        <div className="form-group">
-          <label>Openings</label>
-          <Field name="openings" component={renderField} type="number" className="form-control" />
-        </div>
+      <div className="form-group">
+        <label>Openings</label>
+        <Field name="openings" component={renderField} type="number" className="form-control" />
+      </div>
 
-        <div className="form-group">
-          <label>Category</label>
-          <Field name="category_id" component={SelectionComponent} options={categories} />
-        </div>
+      <div className="form-group">
+        <label>Category</label>
+        <Field name="category_id" component={SelectionComponent} options={categories} />
+      </div>
 
-        <div className="form-group">
-          <label>Description</label>
-          <Field name="description" component={renderField} type="textarea" className="form-control" />
-        </div>
+      <div className="form-group">
+        <label>Description</label>
+        <Field name="description" component={renderField} type="textarea" className="form-control" />
+      </div>
 
-        <div className="form-group">
-          <label>Max Price</label>
-          <Field name="max_price" component={renderField} type="number" className="form-control" />
-        </div>
+      <div className="form-group">
+        <label>Max Price</label>
+        <Field name="max_price" component={renderField} type="number" className="form-control" />
+      </div>
 
-        <div className="form-group">
-          <label>Deadline Date</label>
-          <Field name="deadline" component={renderField} type="date" className="form-control" />
-        </div>
-        <button type="submit" disabled={submitting} className="btn btn-primary"> Submit </button>
-        <div>
-          {error && <strong>{error}</strong>}
-        </div>
-      </form>
-    );
-  }
-}
+      <div className="form-group">
+        <label>Deadline Date</label>
+        <Field name="deadline" component={renderField} type="date" className="form-control" />
+      </div>
+      <button type="submit" disabled={submitting} className="btn btn-primary"> Submit </button>
+      <div>
+        {error && <strong>{error}</strong>}
+      </div>
+    </form>
+  );
+};
+
 
 CreateJobForm = reduxForm({
   form: 'CreateJobForm'
