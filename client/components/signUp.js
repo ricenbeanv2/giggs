7890059ce91f4608d3964556bc00a7baf49a6d8c
Calvin Lee <calsmlee@uw.edu>
2016-09-22 16:27:41 -0700

    [feature] Refactored signUp and createJob to stateless functional components

--- client/components/signUp.js
+++ client/components/signUp.js
@@ -4,63 +4,62 @@ import { Field, reduxForm } from 'redux-form';
 import { userSignUp } from '../actions/auth';
 import renderField from './renderField';
 
-class SignUpForm extends Component {
-  render() {
-    let passCheck = <div />;
-    let loading = '';
-    const { error, handleSubmit, submitting } = this.props;
-    if (submitting) {
-      loading = 'https://thomas.vanhoutte.be/miniblog/wp-content/uploads/light_blue_material_design_loading.gif';
-      passCheck = <div />;
-    }
-    if (typeof this.props.auth === 'string' && this.props.auth.includes('Passwords')) {
-      passCheck = <div>{this.props.auth}</div>;
-    } else {
-      console.log('inside else in signupform');
-      passCheck = <div />;
-    }
-    return (
-        <form onSubmit={handleSubmit(this.props.userSignUp)}>
-          <h3>Sign Up</h3>
-          <div className="form-group">
-            <label>Username</label>
-            <Field name="username" component={renderField} type="text" className="form-control" />
-          </div>
-
-          <div className="form-group">
-            <label>Name</label>
-            <Field name="name" component={renderField} type="text" className="form-control" />
-          </div>
-
-          <div className="form-group">
-            <label>E-mail</label>
-            <Field name="email" component={renderField} type="text" className="form-control" />
-          </div>
-
-          <div className="form-group">
-            <label>Phone Number</label>
-            <Field name="phone" component={renderField} type="number" className="form-control" />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <Field name="password" component={renderField} type="password" className="form-control" />
-          </div>
-
-          <div className="form-group">
-            <label>Confirm Password</label>
-            <Field name="passconfirm" component={renderField} type="password" className="form-control" />
-          </div>
-          {error && <strong>{error}</strong>}
-          <div>
-            <button type="submit" disabled={submitting}>Sign Up</button>
-          </div>
-          <img src={loading} />
-          {passCheck}
-        </form>
-    );
+let SignUpForm = props => {
+  let passCheck = <div />;
+  let loading = '';
+  const { error, handleSubmit, submitting } = props;
+  if (submitting) {
+    loading = 'https://thomas.vanhoutte.be/miniblog/wp-content/uploads/light_blue_material_design_loading.gif';
+    passCheck = <div />;
   }
-}
+  if (typeof props.auth === 'string' && props.auth.includes('Passwords')) {
+    passCheck = <div>{props.auth}</div>;
+  } else {
+    console.log('inside else in signupform');
+    passCheck = <div />;
+  }
+  return (
+      <form onSubmit={handleSubmit(props.userSignUp)}>
+        <h3>Sign Up</h3>
+        <div className="form-group">
+          <label>Username</label>
+          <Field name="username" component={renderField} type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Name</label>
+          <Field name="name" component={renderField} type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>E-mail</label>
+          <Field name="email" component={renderField} type="text" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Phone Number</label>
+          <Field name="phone" component={renderField} type="number" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <Field name="password" component={renderField} type="password" className="form-control" />
+        </div>
+
+        <div className="form-group">
+          <label>Confirm Password</label>
+          <Field name="passconfirm" component={renderField} type="password" className="form-control" />
+        </div>
+        {error && <strong>{error}</strong>}
+        <div>
+          <button type="submit" disabled={submitting}>Sign Up</button>
+        </div>
+        <img src={loading} />
+        {passCheck}
+      </form>
+  );
+};
+
 
 function mapStateToProps({ auth }) {
   return { auth };
