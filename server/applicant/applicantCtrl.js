f828fbc4125fa367799d77270cdc95a1401147bd
tiffanyip <tiffanyip@ucla.edu>
2016-09-23 09:17:12 -0700

    [server-feat] add funciton to cancel application

--- server/applicant/applicantCtrl.js
+++ server/applicant/applicantCtrl.js
@@ -1,19 +1,38 @@
 const Applicant = require('./applicantModel');
+const JOB_STATUS = ['pending', 'accepted', 'rejected'];
 
 module.exports = {
 	applyJob: (req, res) => {
-		const applicant = {
+		const entry = {
 			user_id: req.body.user_id,
 			job_id: req.body.job_id,
 			bid_price: req.body.bid_price,
-			job_status: 'pending'
+			job_status: JOB_STATUS[0]
 		};
-		
-		res.status(200).send('applicant ctrl');
+		Applicant.create(entry)
+		.then(data => {
+			if (data) {
+				res.status(200).json(data);
+			}
+			if (!data) {
+				res.status(200).send('failed to add applicant entry');
+			}
+		});
 	},
 
 	cancelApplied: (req, res) => {
-		res.status(200).send('applicant ctrl');
+		const entry = {
+			user_id: req.body.user_id,
+			job_id: req.body.job_id,
+		};
+		Applicant.cancel(entry)
+		.then(data => {
+			if (data) {
+				res.status(200).send('application canceled');
+			} else {
+				res.status(200).send('failed to cancel application');
+			}
+		});
 	},
 
 	getApplied: (req, res) => {
--- server/applicant/applicantModel.js
+++ server/applicant/applicantModel.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-const connection = require('../db/connection.js');
-const Users = require('../user/userModel.js');
-const Jobs = require('../job/jobModel.js');
+const connection = require('../db/connection');
+const Users = require('../user/userModel');
+const Jobs = require('../job/jobModel');
 
 const Applicant = connection.define('Applicants', {
 	user_id: {
@@ -9,7 +9,8 @@ const Applicant = connection.define('Applicants', {
 		references: {
 			model: Users,
 			key: 'id',
-		}
+		},
+		allowNull: false
 	},
 
 	job_id: {
@@ -17,15 +18,18 @@ const Applicant = connection.define('Applicants', {
 		references: {
 			model: Jobs,
 			key: 'id',
-		}
+		},
+		allowNull: false
 	},
 
 	bid_price: {
 		type: Sequelize.INTEGER,
+		allowNull: false
 	},
 
 	job_status: {
 		type: Sequelize.ENUM('pending', 'accepted', 'rejected'),
+		allowNull: false
 	},
 }, {
 	freezeTableName: true,
@@ -43,7 +47,7 @@ Applicant.create = params => {
 		.then(entry => {
 			if (entry) {
 				//check if user already applied
-				reject('user already applied for the job');
+				reject(entry);
 			}
 			if (!entry) {
 				//create a new entry in applicants if not applied
@@ -51,6 +55,9 @@ Applicant.create = params => {
 				.then(result => {
 					resolve(result);
 				})
+				.catch(err => {
+					reject(err);
+				});
 			}
 		})
 		.catch(err => {
@@ -59,5 +66,26 @@ Applicant.create = params => {
 	});
 };
 
+Applicant.cancel = params => {
+	return new Promise((resolve, reject) => {
+		Applicant.findOne({
+			where: {
+				job_id: params.job_id,
+				user_id: params.user_id
+			}
+		})
+		.then(entry => {
+			if (entry) {
+				entry.destroy()
+				.then(() => {
+					resolve();
+				});
+			}
+		})
+		.catch(err => {
+			reject(err);
+		});
+	});
+};
 
 module.exports = Applicant;
