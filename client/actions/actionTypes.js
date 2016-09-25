36a3995b95506554560aba3bb0cb9ce86f6f4614
Tiffany Ip <tiffanyip@ucla.edu>
2016-09-24 21:29:07 -0700

    Revert "[feature] job listings"

--- client/actions/actionTypes.js
+++ client/actions/actionTypes.js
@@ -7,4 +7,3 @@ export const FB_SIGNUP = 'fb_signup';
 export const UPDATE_USER = 'update_user';
 export const GET_USER = 'get_user';
 export const LOGGED_OUT = 'logged_out';
-export const GET_JOBS = 'get_jobs';
