e60e979a682c1dadc74b65ed1f932233c5b25063
tiffanyip <tiffanyip@ucla.edu>
2016-09-27 15:38:04 -0700

    [client-feat] render details of job entry

--- client/components/jobs/selectedJob.js
+++ client/components/jobs/selectedJob.js
@@ -1,3 +1,5 @@
+import Moment from 'moment';
+
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -29,15 +31,17 @@ class SelectedJob extends Component {
         <button className="btn btn-secondary" onClick={this.clickHandler}>
           Get
         </button> <br />
-        Job Name: {this.state.job.jobName} <br />
-        Openings: {this.state.job.openings} <br />
-        Category: {this.state.category} <br />
-        description: {this.state.job.description} <br />
-        location_lat: {this.state.job.location_lat} <br />
-        location_lng: {this.state.job.location_lng} <br />
-        Max Price: ${this.state.job.max_price} <br />
-        Job Created: {this.state.job.createdAt} <br />
-        Deadline: {this.state.job.deadline} <br />
+        <h4> Job Name: </h4> {this.state.job.jobName} <br />
+        <h4> Openings: </h4> {this.state.job.openings} <br />
+        <h4> Address: </h4> {this.state.job.address} <br />
+        <h4> Category: </h4> {this.state.category.toUpperCase()} <br />
+        <h4> Description: </h4>{this.state.job.description} <br />
+        <h4> location_lat: </h4> {this.state.job.location_lat} <br />
+        <h4> location_lng: </h4>{this.state.job.location_lng} <br />
+        <h4> Max Price: </h4> ${this.state.job.max_price} <br />
+        <h4> Job Created: </h4>{Moment(this.state.job.createdAt).format('LLL')} <br />
+        <h4> Deadline: </h4>{Moment(this.state.job.deadline).format('LLL')} <br />
+        <button className="bnt btn-important">Apply</button>
       </div>
     );
   }
