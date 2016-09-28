9d8e71e5c7bcea66b126907af85cb521c28f5029
Calvin Lee <calsmlee@uw.edu>
2016-09-28 15:21:02 -0700

    [fix] Fixed longitude property typo

--- client/actions/jobs.js
+++ client/actions/jobs.js
@@ -33,7 +33,6 @@ export function getLatLong(address) {
         key: 'AIzaSyAJu6SvKcz7H7fNJb-akc4PJ7BYhlbhqAw'
       }
     }).then((response) => {
-      console.log('Geolocation response:', response.data.results[0].geometry.location);
       dispatch({ type: GET_LAT_LONG, payload: response.data.results[0].geometry.location });
     });
   };
@@ -69,7 +68,7 @@ export function getJobList() {
       .catch(() => {
         throw new SubmissionError({ _error: 'something terrible happened' });
       });
-    }
+    };
 }
 
 export function getJobDetail(jobID) {
