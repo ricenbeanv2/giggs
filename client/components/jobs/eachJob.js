ae955fce05519d2799c0c8cf8f4897ef7e8675d5
Calvin Lee <calsmlee@uw.edu>
2016-09-28 12:24:20 -0700

    [feature] Converts address to lat/long after selecting an address

--- client/components/jobs/eachJob.js
+++ client/components/jobs/eachJob.js
@@ -3,6 +3,7 @@ import React from 'react';
 const eachJob = (props) => {
   return (
     <div>{
+<<<<<<< d609b7320de614ce3144db7a9878fd186f80142f
         props.data.map((eachJob, index) => {
           return <div key={index} className='well'>
             <h4>Job</h4>
@@ -21,6 +22,26 @@ const eachJob = (props) => {
             <p>{eachJob.deadline.toString().slice(0,15)}</p>
           </div>
         })
+=======
+      props.data.map((eachJob, index) => {
+        return <div key={index} className='well'>
+          <h4>Job</h4>
+          <p>{eachJob.jobName}</p>
+          <h4>Category</h4>
+          <p>{eachJob.category_id}</p>
+          <h4>Location</h4>
+          <p>{eachJob.location_lat}</p>
+          <h4>Description</h4>
+          <p>{eachJob.description}</p>
+          <h4>Openings</h4>
+          <p>{eachJob.openings}</p>
+          <h4>Max Price</h4>
+          <p>{eachJob.max_price}</p>
+          <h4>Deadline</h4>
+          <p>{eachJob.deadline.toString()}</p>
+        </div>
+      })
+>>>>>>> [feature] Converts address to lat/long after selecting an address
       }</div>
   );
 };
--- client/reducers/jobReducer.js
+++ client/reducers/jobReducer.js
@@ -1,6 +1,6 @@
-import { GET_ALL_JOBS, CREATE_JOB, GET_JOBS, GET_CATEGORY, SORT_PRICE, SORT_CATEGORIES, SORT_DATE, FILTER_CATEGORY } from '../actions/actionTypes';
+import { GET_ALL_JOBS, CREATE_JOB, GET_JOBS, GET_CATEGORY, SORT_PRICE, SORT_CATEGORIES, SORT_DATE, FILTER_CATEGORY, GET_LAT_LONG } from '../actions/actionTypes';
 
-const INITIAL_STATE = { job: [], jobList: [], created: [], category: [] };
+const INITIAL_STATE = { job: [], joblist: [], created: [], category: [], latLong: {} };
 
 export default function (state = INITIAL_STATE, { type, payload }) {
   switch (type) {
@@ -10,16 +10,18 @@ export default function (state = INITIAL_STATE, { type, payload }) {
       return { ...state, job: payload };
     case GET_ALL_JOBS:
       return { ...state, jobList: payload };
+    case GET_LAT_LONG:
+      return { ...state, latLong: payload };
     case GET_CATEGORY:
       return { ...state, category: payload };
     case SORT_CATEGORIES:
-      return {...state, jobList: payload}
+      return { ...state, jobList: payload };
     case SORT_PRICE:
-      return {...state, jobList: payload}
+      return { ...state, jobList: payload };
     case SORT_DATE:
-      return {...state, jobList: payload}
+      return { ...state, jobList: payload };
     case FILTER_CATEGORY:
-      return {...state, jobList: payload}
+      return { ...state, jobList: payload };
     default:
       return state;
   }
