73ab81010e89b3c29bbf73c30007f9f0c5a56ac4
Calvin Lee <calsmlee@uw.edu>
2016-09-28 12:24:20 -0700

    [feature] Converts address to lat/long after selecting an address

--- client/components/jobs/eachJob.js
+++ client/components/jobs/eachJob.js
@@ -30,16 +30,31 @@ const eachJob = (props) => {
           <h4>Category</h4>
           <p>{eachJob.category_id}</p>
           <h4>Location</h4>
+<<<<<<< 79647d7fa0e8cb96ee12b507eb502ed037bf84ab
           <p>{eachJob.location_lat}</p>
+=======
+          <p>{eachJob.address}</p>
+>>>>>>> [feature] Converts address to lat/long after selecting an address
           <h4>Description</h4>
           <p>{eachJob.description}</p>
           <h4>Openings</h4>
           <p>{eachJob.openings}</p>
           <h4>Max Price</h4>
           <p>{eachJob.max_price}</p>
+<<<<<<< 79647d7fa0e8cb96ee12b507eb502ed037bf84ab
           <h4>Deadline</h4>
           <p>{eachJob.deadline.toString()}</p>
         </div>
+=======
+          
+          <p>{eachJob.deadline.toString()}</p>
+        </div>
+        })
+=======
+          <h4>Deadline</h4>
+          <p>{eachJob.deadline.toString()}</p>
+        </div>
+>>>>>>> [feature] Converts address to lat/long after selecting an address
       })
 >>>>>>> [feature] Converts address to lat/long after selecting an address
       }</div>
