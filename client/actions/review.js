65e7fbff6b8a675a87c8aa7a37c7048dd5975ec8
justsandytran <just.sandy.tran@gmail.com>
2016-10-03 11:42:12 -0700

    [bug] fix bug

--- client/actions/review.js
+++ client/actions/review.js
@@ -3,19 +3,19 @@ import Cookies from 'js-cookie';
 import { SubmissionError } from 'redux-form';
 import { browserHistory } from 'react-router';
 
-import { GET_REVIEWS, CREATE_REVIEW } from './actionTypes';
+import { CREATE_REVIEW, GET_REVIEWS } from './actionTypes';
 
 export function createReview (reviewProp) {
   console.log("inside createReview:", reviewProp);
   return (dispatch) => {
-    dispatch({type:CREATE_REVIEW, payload: []})
+    dispatch({type: CREATE_REVIEW, payload: []})
   }
-}
+};
 
 
 export function getReviews () {
   console.log("inside getReviews:");
   return (dispatch) => {
-    dispatch({type:GET_REVIEW, payload: []})
+    dispatch({type:GET_REVIEWS, payload: []})
   }
-}
+};
