da9f9c9e01d7174d2a83a2811f4071711eb523a5
tiffanyip <tiffanyip@ucla.edu>
2016-10-03 16:28:58 -0700

    [client-feat] add button to redirect user to job page

--- client/components/account/userApplications.js
+++ client/components/account/userApplications.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import Moment from 'moment';
 import { connect } from 'react-redux';
+import { browserHistory } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { getUserApps } from '../../actions/auth.js';
+import { getJobDetail } from '../../actions/jobs.js';
 
 class UserApplications extends Component {
   constructor(props) {
     super(props);
     this.renderEachApp = this.renderEachApp.bind(this);
+    this.redirectToJob = this.redirectToJob.bind(this);
   }
   componentWillMount() {
     this.props.getUserApps();
+  }
+  redirectToJob(jobID) {
+    this.props.getJobDetail(jobID).then(() => {
+      browserHistory.push('/selectedJob');
+    });
   }
 
   renderEachApp(applicationData, key) {
@@ -25,13 +34,18 @@ class UserApplications extends Component {
           {applicationData.job_status}
         </td>
         <td>
-          {applicationData.createdAt}
+          {Moment(applicationData.createdAt).format('LLL')}
         </td>
         <td>
-          Go to Job
+          <button
+            className="btn btn-secondary"
+            onClick={() => this.redirectToJob(applicationData.job_id)}
+          >
+            Go to Job
+          </button>
         </td>
       </tr>
-    )
+    );
   }
   render() {
     return (
@@ -39,13 +53,17 @@ class UserApplications extends Component {
         <h4> Your Applications: </h4>
         <table className="table">
           <thead>
-            <th>Job ID </th>
-            <th>Bid Price </th>
-            <th>Job status </th>
-            <th>Applied At </th>
-            <th>Action </th>
+            <tr>
+              <th>Job ID </th>
+              <th>Bid Price </th>
+              <th>Job status </th>
+              <th>Applied At </th>
+              <th>Action </th>
+            </tr>
           </thead>
-          {this.props.auth.userApps.map(this.renderEachApp)}
+          <tbody>
+            {this.props.auth.userApps.map(this.renderEachApp)}
+          </tbody>
         </table>
       </div>
     );
@@ -57,7 +75,7 @@ function mapStateToProps({ auth }) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ getUserApps }, dispatch);
+  return bindActionCreators({ getUserApps, getJobDetail }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserApplications);
