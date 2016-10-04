a6360cb63cdaee34fe669841d8d463a9b5f48825
justsandytran <just.sandy.tran@gmail.com>
2016-10-04 10:08:22 -0700

    [add] change review_id setting it to Cookie.getJSON(user).userid

--- client/components/jobs/reviews/createReview.js
+++ client/components/jobs/reviews/createReview.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { createReview } from '../../../actions/review';
+import Cookies from 'js-cookie';
+
 
 import StarReview from './starReview';
 
@@ -10,7 +12,7 @@ class createReviews extends Component {
     super(props)
     this.state = {
        type: "employer",
-       review_id: 2, //This will be changed!
+       review_id: Cookies.getJSON('user').userid, //This will be changed!
        job_id: 4, //This will also be changed!
        employerReview: "",
        numericalEmployerReview: ""
@@ -22,18 +24,16 @@ class createReviews extends Component {
 
   handleSubmit(event){
     event.preventDefault();
-    console.log('this.state.testing', this.state)
-    this.props.createReview(this.state)
+    this.props.createReview(this.state);
+    console.log("Line 28, Inside handleSubmit", this.state);
   }
   handleChange(event){
     event.preventDefault();
     this.setState({ employerReview : event.target.value})
-    console.log('this.state.employerReview', this.state.employerReview)
   }
    handleStarRate(event){
      event.preventDefault();
      this.setState({numericalEmployerReview: event.target.value})
-     console.log('Inside starReview:', event.target.value)
    }
 
   render() {
