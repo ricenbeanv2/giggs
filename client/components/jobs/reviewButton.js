10e853a82cca2bf3198b8066b33f19e1f6655c42
tiffanyip <tiffanyip@ucla.edu>
2016-10-05 15:38:26 -0700

    [client-feat] modularize review button

--- client/components/jobs/reviewButton.js
+++ client/components/jobs/reviewButton.js
@@ -7,34 +7,6 @@ import { setReviewInfo } from '../../actions/review';
 
 class ReviewButton extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     type: '',
-  //     job_id: this.props.jobs.job.id,
-  //     user_id: Cookies.getJSON('user').userid
-  //   };
-  // }
-
-  // handleRedirect(){
-  //   // if (window.location.pathname === '/jobadmin'){
-  //   //   this.setState({ type: 'employer' });
-  //   // } else {
-  //   //   this.setState({ type: 'employee' });
-  //   // }
-  //   const info = {
-  //     type: this.state.type,
-  //     user_id: this.state.user_id,
-  //     job_id: this.state.job_id,
-  //   };
-  //   this.props.setReviewInfo(info).then(() => {
-  //     browserHistory.push('/createReview');
-  //   });
-  // }
-  //
-  // //if reviewed - see reviewe
-  // //if not reviewed create review
-
   render() {
     return (
       <div>
@@ -47,11 +19,3 @@ class ReviewButton extends Component {
 }
 
 export default ReviewButton;
-
-// function mapStateToProps({ jobs }) {
-//   return { jobs };
-// }
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators({ setReviewInfo }, dispatch);
-// }
-// export default connect(mapStateToProps, )(ReviewButton);
