0e81bd96f1b41544fbd3542fc2975eb1c37ee95a
tiffanyip <tiffanyip@ucla.edu>
2016-10-05 17:21:21 -0700

    [client-feat] modify the create review function

--- client/components/jobs/reviews/createReview.js
+++ client/components/jobs/reviews/createReview.js
@@ -9,33 +9,45 @@ class createReviews extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       type: this.props.reviews.info.type,
-       review_id: Cookies.getJSON('user').userid,
-       job_id: this.props.reviews.info.job_id,
-       employerReview: '',
-       numericalEmployerReview: ''
+       review: '',
+       numericalReview: ''
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleStarRate = this.handleStarRate.bind(this);
   }
 
-componentWillMount() {
-    console.log("this.props.reviews", this.props.reviews);
-}
-handleSubmit(event) {
-    event.preventDefault();
-    console.log("before submitting review", this.state);
-    this.props.createReview(this.state);
-}
-handleChange(event) {
-    event.preventDefault();
-    this.setState({ employerReview: event.target.value });
-}
-handleStarRate(event) {
-    event.preventDefault();
-    this.setState({ numericalEmployerReview: event.target.value });
-}
+  handleSubmit(event) {
+      event.preventDefault();
+      let params = {};
+      if (this.state.type === 'employer') {
+        params = {
+          type: this.props.reviews.info.type,
+          review_id: Cookies.getJSON('user').userid,
+          job_id: this.props.reviews.info.job_id,
+          employerReview: this.state.review,
+          numericalEmployerReview: this.state.numericalReview
+        };
+        this.props.createReview(params);
+      } else {
+        params = {
+          type: this.props.reviews.info.type,
+          review_id: Cookies.getJSON('user').userid,
+          job_id: this.props.reviews.info.job_id,
+          employeeReview: this.state.review,
+          numericalEmployeeReview: this.state.numericalReview
+        };
+        this.props.createReview(params);
+      }
+  }
+  handleChange(event) {
+      event.preventDefault();
+      this.setState({ review: event.target.value });
+  }
+  handleStarRate(event) {
+      event.preventDefault();
+      this.setState({ numericalReview: event.target.value });
+  }
   render() {
     return (
       <div>
