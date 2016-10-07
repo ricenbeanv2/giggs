d01b679bb6dffdbe277bfe294db92ad8f75077e6
justsandytran <just.sandy.tran@gmail.com>
2016-10-07 12:30:02 -0700

    [bug] star rating bug

--- client/reducers/reviewReducer.js
+++ client/reducers/reviewReducer.js
@@ -1,18 +1,17 @@
-import { GET_EMPLOYER_REVIEWS, GET_EMPLOYEE_REVIEWS, CREATE_REVIEW, REVIEW_INFO, IS_REVIEWED, GET_STAR_RATING } from '../actions/actionTypes';
+import { GET_EMPLOYER_REVIEWS, GET_EMPLOYEE_REVIEWS, CREATE_REVIEW, REVIEW_INFO, IS_REVIEWED, GET_EMPLOYEE_STAR_RATING, GET_EMPLOYER_STAR_RATING } from '../actions/actionTypes';
 
 const INITIAL_STATE = { getEmployee: [], getEmployer: [], create: [], info: '', isReviewed: '' , starRating: 0};
 
 export default function (state = INITIAL_STATE, { type, payload }) {
 	switch (type) {
 		case GET_EMPLOYER_REVIEWS:
-		console.log('Inside GET_EMPLOYER_REVIEWS reducer:', payload)
 			return { ...state, getEmployer: payload };
 		case GET_EMPLOYEE_REVIEWS:
-		console.log('Inside GET_EMPLOYEE_REVIEWS reducer:', payload)
 			return { ...state, getEmployee: payload };
-		case GET_STAR_RATING:
-		console.log('Inside GET_STAR_RATING reducer:', payload)
-			return { ...state, starRating: payload };
+		case GET_EMPLOYEE_STAR_RATING:
+			return { ...state, EeStarRating: payload };
+		case GET_EMPLOYER_STAR_RATING:
+			return { ...state, ErStarRating: payload };
     case CREATE_REVIEW:
       return { ...state, create: payload };
 		case REVIEW_INFO:
