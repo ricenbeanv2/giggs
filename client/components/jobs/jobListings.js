eec83356996d0ad1b225a350871beaa9b094dde3
justsandytran <just.sandy.tran@gmail.com>
2016-10-07 18:03:21 -0700

    [feature] commit update

--- client/components/jobs/jobListings.js
+++ client/components/jobs/jobListings.js
@@ -34,16 +34,18 @@ class JobListings extends Component {
   render() {
     return (
       <div className='jobListings'>
-        <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1">
-          <MenuItem eventKey="1" onClick={this.props.sortPriceChange}>Price</MenuItem>
-          <MenuItem eventKey="2" onClick={this.props.sortCategories}>Category</MenuItem>
-          <MenuItem eventKey="2" onClick={this.props.sortDate}>Recent Post</MenuItem>
-        </DropdownButton>
         <center className='searchBar'>
           <form className='inputForm'>
             <input className='inputBar' type='text' placeholder='Search category' value={this.state.changes} onChange={this.handleChanges}/>
           </form>
-          <button className='inputButton' onClick={this.handleSubmit}>Submit</button>
+          <button className="btn btn-secondary" onClick={this.handleSubmit}>Submit</button>
+        </center>
+        <center className='dropDownCenter'>
+          <DropdownButton title="Dropdown" id="bg-vertical-dropdown-1" className='dropDownBar'>
+            <MenuItem eventKey="1" onClick={this.props.sortPriceChange}>Price</MenuItem>
+            <MenuItem eventKey="2" onClick={this.props.sortCategories}>Category</MenuItem>
+            <MenuItem eventKey="2" onClick={this.props.sortDate}>Recent Post</MenuItem>
+          </DropdownButton>
         </center>
         <EachJob data={this.props.jobs.jobList} />
       </div>
