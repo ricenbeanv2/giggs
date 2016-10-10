ba9b563c0799e9e7f98fd32d4bd57a6c463010c3
Calvin Lee <calsmlee@uw.edu>
2016-10-09 17:34:24 -0700

    [style] Styled navbar still in progress

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -5,52 +5,58 @@ import { Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
 
 import LogOut from './account/logout';
 
-class NavDropdownBar extends Component {
-  handleSelect(eventKey) {
-    eventKey.preventDefault();
-    alert(`selected ${eventKey}`);
-  }
-
-  render() {
-    return (
-      <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
-        <NavDropdown eventKey="4" title="Dropdown" id="nav-dropdown">
-          <MenuItem eventKey="4.1">Action</MenuItem>
-          <MenuItem eventKey="4.2">Another action</MenuItem>
-          <MenuItem eventKey="4.3">Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey="4.4">Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-    );
-  }
-}
+// class NavDropdownBar extends Component {
+//   handleSelect(eventKey) {
+//     // eventKey.preventDefault();
+//     // alert(`selected ${eventKey}`);
+//   }
+//
+//   render() {
+//     return (
+//       <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
+//         <NavDropdown eventKey="4" title="Dropdown" id="nav-dropdown">
+//           <MenuItem eventKey="4.1">Action</MenuItem>
+//           <MenuItem eventKey="4.2">Another action</MenuItem>
+//           <MenuItem eventKey="4.3">Something else here</MenuItem>
+//           <MenuItem divider />
+//           <MenuItem eventKey="4.4">Separated link</MenuItem>
+//         </NavDropdown>
+//       </Nav>
+//     );
+//   }
+// }
 
 const NavBar = () => {
   if (Cookies.getJSON('token')) {
     return (
-      <nav className="navbar navbar-default">
-        <ul className="nav navbar-nav">
-          <li><Link to="/">Giggs </Link></li>
-          <li><Link to="/joblistings">Job Listings </Link></li>
-          <li><Link to="/map">Map</Link></li>
-          <li><Link to="/createjob">Create Job </Link></li>
-          <li><Link to="/userprofile">User Profile </Link></li>
-          <li><Link to="/profile">Public Profile</Link></li>
-          <li><Link to="/payment">Payment</Link></li>
-          <li><Link to="/chat">Chat </Link></li>
-          <li><LogOut /></li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <NavDropdownBar />
-          {/* <ul className="nav navbar-nav navbar-right">
-            <li><Link><i className="fa fa-user fa-1x" aria-hidden="true"></i> { Cookies.getJSON('user').username[0].toUpperCase() + Cookies.getJSON('user').username.slice(1)}</Link>
-            </li>
+      <header className="header">
+        <div className="container">
+          <div className="row">
+            <nav className="desktop-navigation-wrap">
+              <ul className="desktop-navigation">
+                <li><Link to="/">Giggs</Link></li>
+                <li><Link to="/joblistings">Job Listings </Link></li>
+                <li><Link to="/map">Map</Link></li>
+                <li><Link to="/createjob">Create Job </Link></li>
+                <li><Link to="/userprofile">User Profile </Link></li>
+                <li><Link to="/profile">Public Profile</Link></li>
+                <li><Link to="/payment">Payment</Link></li>
+                <li><Link to="/chat">Chat </Link></li>
+                <li><LogOut /></li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                {/* <NavDropdownBar /> */}
+                {/* <ul className="nav navbar-nav navbar-right">
+                  <li><Link><i className="fa fa-user fa-1x" aria-hidden="true"></i> { Cookies.getJSON('user').username[0].toUpperCase() + Cookies.getJSON('user').username.slice(1)}</Link>
+                  </li>
 
-          </ul> */}
-        </ul>
-      </nav>
-        );
+                </ul> */}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
   }
   return (
     <nav className="navbar navbar-default">
